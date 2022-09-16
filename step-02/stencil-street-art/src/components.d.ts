bf966abb9f3862707255139cddde1d508c0936f2
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2022-09-16 11:48:17 +0200

    Updating Stencil version

--- step-02/stencil-street-art/src/components.d.ts
+++ step-02/stencil-street-art/src/components.d.ts
@@ -6,6 +6,11 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
+    interface ArtListItem {
+        "coordinates": string;
+        "link": string;
+        "location": string;
+    }
     interface MyComponent {
         /**
           * The first name
@@ -22,6 +27,12 @@ export namespace Components {
     }
 }
 declare global {
+    interface HTMLArtListItemElement extends Components.ArtListItem, HTMLStencilElement {
+    }
+    var HTMLArtListItemElement: {
+        prototype: HTMLArtListItemElement;
+        new (): HTMLArtListItemElement;
+    };
     interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
     }
     var HTMLMyComponentElement: {
@@ -29,10 +40,16 @@ declare global {
         new (): HTMLMyComponentElement;
     };
     interface HTMLElementTagNameMap {
+        "art-list-item": HTMLArtListItemElement;
         "my-component": HTMLMyComponentElement;
     }
 }
 declare namespace LocalJSX {
+    interface ArtListItem {
+        "coordinates"?: string;
+        "link"?: string;
+        "location"?: string;
+    }
     interface MyComponent {
         /**
           * The first name
@@ -48,6 +65,7 @@ declare namespace LocalJSX {
         "middle"?: string;
     }
     interface IntrinsicElements {
+        "art-list-item": ArtListItem;
         "my-component": MyComponent;
     }
 }
@@ -55,6 +73,7 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
+            "art-list-item": LocalJSX.ArtListItem & JSXBase.HTMLAttributes<HTMLArtListItemElement>;
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
         }
     }
--- step-02/stencil-street-art/src/components/art-list-item/art-list-item.tsx
+++ step-02/stencil-street-art/src/components/art-list-item/art-list-item.tsx
@@ -1,6 +1,5 @@
 import { Component, Prop, h } from '@stencil/core';
 
-
 @Component({
   tag: 'art-list-item',
   styleUrl: 'art-list-item.css',
@@ -10,11 +9,11 @@ export class ArtListItem {
   @Prop() location: string;
   @Prop() coordinates: string;
   @Prop() link: string;
-  
+
   render() {
-    return <div class="art">
-        <h2> {this.location} </h2>
-        <p> <a href={this.link}>{this.coordinates}</a></p>
-    </div>;
-  }
+      return <div class="art">
+          <h2> {this.location} </h2>
+          <p> <a href={this.link}>{this.coordinates}</a></p>
+      </div>;
+  }   
 }
